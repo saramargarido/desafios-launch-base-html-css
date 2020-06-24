f58fef33fea92e413149c23a84a25f916d9a7972
saramargarido <saramargarido@gmail.com>
2020-06-24 15:43:35 -0300

    modal dos cursos

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.course')
+
+for (let card of cards) {
+    card.addEventListener('click', function(){
+        const pageId = card.getAttribute('id')
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${pageId}`
+    })
+}
+
+document.querySelector(".close-modal").addEventListener('click', function(){
+    modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ""
+})
