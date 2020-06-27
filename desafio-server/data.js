13f14d49fbbf92cfadb208cb63387ee1d5e2b5ab
saramargarido <saramargarido@gmail.com>
2020-06-27 16:21:48 -0300

    consertando cards

---
+++ desafio-server/data.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+        id: "",
+        modules:2,
+        price: ""
+    },
+
+    {
+        id: "",
+        modules:2,
+        price: ""
+    },
+    
+    {
+        id: "",
+        modules:2,
+        price: ""
+    }
+]
