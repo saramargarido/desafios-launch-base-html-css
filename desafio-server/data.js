a9ff296cee67b964f95980a13e31c51e918980e5
saramargarido <saramargarido@gmail.com>
2020-06-29 23:03:56 -0300

    module.exports

--- desafio-server/data.js
+++ desafio-server/data.js
@@ -1,19 +1,8 @@
 module.exports = [
     {
-        id: "",
-        modules:2,
-        price: ""
-    },
-
-    {
-        id: "",
-        modules:2,
-        price: ""
-    },
-    
-    {
-        id: "",
-        modules:2,
-        price: ""
+        id: "starter",
+        description: "Torne-se um programador desejado no mercado com esses cursos gratuitos.",
+        modules: 5,
+        price: "Free"
     }
 ]
